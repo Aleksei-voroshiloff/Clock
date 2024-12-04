ac629657bad02fa8e5375ffd06e936cb0eed2cf0
Aleksei-voroshiloff <alexey909@gmail.com>
2024-12-04 20:03:34 +0300

    routes/midd/cookie

---
+++ server/src/routes/orderRouter.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const express = require('express');
+const { Order } = require('../../db/models/');
+const orderRouter = express.Router();
+
+
+orderRouter.get('/', async (req, res) => {
+  try {
+    const orders = await Order.findAll();
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: 'Ошибка сервера' });
+    console.log(error);
+  }
+})
+
+module.exports = orderRouter;
